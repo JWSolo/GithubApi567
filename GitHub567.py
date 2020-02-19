da43dd2ed7f787819f69c6de326e4ddb20633a14
Jia Wen <jwen6@stevens.edu>
2020-02-19 11:39:34 -0500

    Update GitHub567.py

--- GitHub567.py
+++ GitHub567.py
@@ -25,9 +25,8 @@ class TextFunction(unittest.TestCase):
 
     def test_GitHub567(self):
 
-        self.assertTrue(GitHub567("JWSolo"), ['SSW567', 'Triangle567', 'University-HW10'])
+        self.assertEqual(GitHub567("JWSolo"), ['GithubApi567', 'SSW567', 'Triangle567', 'University-HW10'])
 
 if __name__ == "__main__":
     unittest.main()
-    print(GitHub567("JWSolo"))
 
