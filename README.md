75cb4741c6260ebcd033f9a68e3e371b5bf3ed7b
Jia Wen <jwen6@stevens.edu>
2020-02-26 16:08:44 -0500

    Update Python Mock

--- README.md
+++
@@ -1,1 +0,0 @@
-[![build status of master](https://travis-ci.org/JWSolo/GithubApi567.svg?branch=master)](https://travis-ci.org/JWSolo/GithubApi567)
---
+++ TestApi.py
@@ -0,0 +1,15 @@
+import unittest
+from unittest import mock
+import GitHub567
+
+
+class MyTestCase(unittest.TestCase):
+    @mock.patch('GitHub567.GitHub567')
+    def test_GitHub567(self, mock_req):
+        mock_req("JWSolo").return_value = {'GithubApi567': 21, 'SSW567': 7, 'Triangle567': 8, 'University-HW10': 2}
+        self.assertEqual(mock_req("JWSolo").return_value,
+                         {'GithubApi567': 21, 'SSW567': 7, 'Triangle567': 8, 'University-HW10': 2})
+
+
+if __name__ == '__main__':
+    unittest.main()
